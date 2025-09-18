51bdebdd06552d9c10281e0b2e22ce30c2c09c42
NHG-Donut <149401451+NHG-Donut@users.noreply.github.com>
2025-09-18 22:40:33 +0800

    Create resource.ts

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,17 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 'radarProjectStorage',
+  access: (allow) => ({
+    'public/*': [
+      allow.authenticated.to(['read', 'write', 'delete']),
+      allow.guest.to(['read'])
+    ],
+    'protected/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'private/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
+  })
+});
