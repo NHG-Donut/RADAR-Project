1fd0d1c734fcece833c1c6cd9418255b94b844a3
NHG-Donut <149401451+NHG-Donut@users.noreply.github.com>
2025-09-18 22:15:49 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
